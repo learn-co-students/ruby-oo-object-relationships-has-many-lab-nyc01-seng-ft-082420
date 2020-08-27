78de3edc1e1de5e5e3e4eb4081f802a80647463c
Eddy_Yunhao_Gao <as510891328@gmail.com>
2020-08-26 22:12:29 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+require "pry"
+class Post
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title
+    save
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    self.author.name if author
+  end
+end
