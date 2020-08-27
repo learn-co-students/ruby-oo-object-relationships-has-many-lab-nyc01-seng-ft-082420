bb1597b9259c47a12df1db1b52b465220d3359d4
sdonohue0918 <sdonohue0918@gmail.com>
2020-08-27 08:51:39 -0400

    all rspec passing for Has Many Object Lab

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+
+require_relative '../lib/artist.rb'
+require 'pry'
+
+class Song
+    
+    @@all = []
+
+    attr_accessor :name, :artist
+    
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artist_name
+        artist.name if artist
+
+    end
+end
